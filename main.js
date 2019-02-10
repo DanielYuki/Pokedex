e408983a2e0034b7674dbe0b6c3b599d2e6e0976
Dyuki <unblivion@gmail.com>
2019-02-09 23:02:36 -0200

    ios modal only/ improvements coming soon

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 
 
 
+// Detects if device is on iOS 
+const isIos = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test(userAgent);
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+    let showModal = document.querySelector('.iosAlertBoxWebApp');
+    showModal.style.animation = 'show 8s ease-in-out 2s'
+    showModal.style.display = 'flex'
+    setTimeout(timeOutModal = () => { showModal.style.display = 'none' }, 10000)
+}
+
 // service worker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
