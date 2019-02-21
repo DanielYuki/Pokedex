12c44b7597c4d04bc43dc687c6f44824b17e06a7
Dyuki <unblivion@gmail.com>
2019-02-21 19:21:10 -0300

    better css / imrpovements coming soon

--- main.js
+++ main.js
@@ -9,11 +9,21 @@ showTypeFilter.onclick = () => {
     let btnDetail = document.querySelector('#arrowBtn');
     btnDetail.classList.toggle('rotate')
 }
+
+let typeFiltering = document.querySelectorAll('.typeFiltering');
+for (types of typeFiltering) {
+    types.onclick = filterThisType;
+}
+
+function filterThisType() {
+    console.log('kasodk')
+    this.classList.toggle('grayScaleFiltering')
+}
 // const allGen1Pkm = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise', 'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata', 'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash', 'Nidoran♀', 'Nidorina', 'Nidoqueen', 'Nidoran♂', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales', 'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 'Venonat', 'Venomoth', 'Diglett', 'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape', 'Growlithe', 'Arcanine', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke', 'Machamp', 'Bellsprout', 'Weepinbell', 'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem', 'Ponyta', 'Rapidash', 'Slowpoke', 'Slowbro', 'Magnemite', 'Magneton', "Farfetch'd", 'Doduo', 'Dodrio', 'Seel', 'Dewgong', 'Grimer', 'Muk', 'Shellder', 'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 'Krabby', 'Kingler', 'Voltorb', 'Electrode', 'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak', 'Hitmonlee', 'Hitmonchan', 'Lickitung', 'Koffing', 'Weezing', 'Rhyhorn', 'Rhydon', 'Chansey', 'Tangela', 'Kangaskhan', 'Horsea', 'Seadra', 'Goldeen', 'Seaking', 'Staryu', 'Starmie', 'Mr.Mime', 'Scyther', 'Jynx', 'Electabuzz', 'Magmar', 'Pinsir', 'Tauros', 'Magikarp', 'Gyarados', 'Lapras', 'Ditto', 'Eevee', 'Vaporeon', 'Jolteon', 'Flareon', 'Porygon', 'Omanyte', 'Omastar', 'Kabuto', 'Kabutops', 'Aerodactyl', 'Snorlax', 'Articuno', 'Zapdos', 'Moltres', 'Dratini', 'Dragonair', 'Dragonite', 'Mewtwo', 'Mew'];
 let allPkmTypes = ['normal', 'grass', 'fire', 'water', 'fightning', 'flying', 'poison', 'ground', 'rock', 'bug', 'ghost', 'electric', 'psychic', 'ice', 'dragon', 'dark', 'steel', 'fairy']
 
 function filterFunction() {
-    let filter, li, txtValue, name, number, type,typeFilter = '';
+    let filter, li, txtValue, name, number, type, typeFilter = '';
     filter = searchInput.value.toLowerCase().replace(/\s/g, '');
     li = pkmCardsList.getElementsByTagName("li");
     //FILTER TYPES
@@ -93,7 +103,7 @@ function pkmCreator(name, sprite, type, number, spriteBack, height, weight) {
     createPkmNumber.innerText = (`#${number}`);
     let createPkmHeightValue = document.createElement('div');
     let createPkmWeightValue = document.createElement('div');
-        //details
+    //details
     let createPkmHeight = document.createElement('b');
     createPkmHeight.innerText = 'Height: '
     let createPkmWeight = document.createElement('b');
